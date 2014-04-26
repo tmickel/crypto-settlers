404429d903860d1dbcdffb79a819a9973258b65f
Hassan Alsibyani <sibyani@mit.edu>
2014-04-26 14:38:37 -0400

    Board function
    Shuffling and AES encrypting the tiles.

---
+++ board.py
@@ -0,0 +1,19 @@
+from Crypto.Cipher import AES
+from Crypto import Random
+import random
+
+def board(boardList):
+    random.shuffle(boardList)
+
+    key = Random.new().read(AES.block_size)
+    iv = Random.new().read(AES.block_size)
+    cipher = AES.new(key, AES.MODE_CFB, iv)
+    shuffledEncryptedList = []
+    
+    print boardList
+    for i in xrange(len(boardList)):
+        encryption = cipher.encrypt(str(boardList[i]))
+        shuffledEncryptedList.append(encryption)
+        
+    return shuffledEncryptedList
+    
